e18949f09f10818266cf3b998e97fcf15c1be681
Agnik Saha <agniksaha2019@gmail.com>
2021-05-22 10:02:53 +0530

    add upload img

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 import cv2
 import  camerahelper 
+import predictEmo
+
 app = Flask(__name__)
 
 
@@ -12,8 +14,26 @@ def gen():  # generate frame by frame from camera
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-        
-           
+
+
+@app.route('/')
+def hello():
+    return render_template("index2.html")
+
+@app.route('/', methods = ['POST'])
+def cap():
+    if request.method == 'POST':
+        f = request.files['userfile']
+        path = "./static/{}".format(f.filename)
+        f.save(path)
+
+    emo = predictEmo.predictImg(path)
+    result_dic = {
+        'image':path,
+        'emotion': emo
+    }
+
+    return render_template("index2.html", your_result = result_dic )       
 
 @app.route('/video_feed')
 def video_feed():
@@ -21,7 +41,7 @@ def video_feed():
     return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-@app.route('/')
+@app.route('/cam')
 def index():
     """Video streaming home page."""
     return render_template('index.html')
